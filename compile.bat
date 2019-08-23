67c3bdf0cb596c1d8f982b7fdaca8b3c798c02c4
Brandon Pace <brandonmpace@gmail.com>
2019-08-22 21:41:30 -0500

    Remove some lines that were preventing the program from working when called with subprocess in Python

--- compile.bat
+++ compile.bat
@@ -1,2 +1,5 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\vsvars32.bat"
 csc.exe file2clip.cs 
+
+@REM Need to find out what vsvars32.bat contained and find equivalent in 2017 Community edition.. maybe it's now C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\vsdevcmd\ext\vcvars.bat
+@REM my csc.exe is under C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\Roslyn\
